e34667b05a87ba401c9f1eadc74a4c5703ca2c34
Andrew <andjuu.barnes@gmail.com>
2018-06-12 22:11:06 +0100

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+ReactDOM.render(
+  <div>It's alive!</div>,
+  document.getElementById('root') as HTMLElement
+)
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: "ts-loader" }
+    ]
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist"),
+  }
+};
